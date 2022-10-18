bc81d4ecdd5913edc0d465b057ace83e884a1dc4
patro <patro@112.161.194.158>
2022-10-18 23:01:33 +0900

    통계자료적재 화면 개선

--- src/main/resources/static/js/app/posts-save.js
+++ src/main/resources/static/js/app/posts-save.js
@@ -11,49 +11,52 @@ var main = {
 
     },
     save1 : function () {
+        const staticId = $('#selStatic option:selected').val();
+        const staticText = $('#selStatic option:selected').text();
+        const startYyyymm =  $('#startYyyymm').val().replace('-','');
+        const endYyyymm =  $('#endYyyymm').val().replace('-','');
+        
+        if(startYyyymm.length != 6
+        || endYyyymm.length != 6
+        ) {
+	        alert("시작년월, 종료년월이 부적합합니다.");
+	        return;
+		}
+        const chkMessage = staticText + " " 
+        + startYyyymm + "~"
+        + endYyyymm + " 적재하시겠습니까?";
+        const chk = confirm(chkMessage);
+        
+        if(chk == false) return;
+        
         var data = {
-            startYyyymm: $('#startYyyymm').val() ,
-            endYyyymm: $('#endYyyymm').val() ,
+            startYyyymm: startYyyymm ,
+            endYyyymm: endYyyymm ,
         };
-
+		
+		const prcUrl = '/population/'+ staticId;
+		$('#btn-save1').addClass('disabled');
+		$('#loadingbar').css('display','block');
         $.ajax({
             type: 'POST',
-            url: '/population/loadStatPopulationTrendSido',
+            url: prcUrl,
             dataType: 'text',
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function(response){
+			$('#loadingbar').css('display','none');
+			$('#btn-save1').removeClass('disabled');
 			var data = JSON.parse(response).data;
 			console.log(data);
             alert('정상처리되었습니다.' + ' : ' + data);            
             //window.location.href = '/';
         }).fail(function(response) {
+			$('#loadingbar').css('display','none');
+			$('#btn-save1').removeClass('disabled');
             var error = JSON.parse(response.responseText);
             alert(error.code + ' : ' + error.message);
         });
     },
-    save2 : function () {
-        var data = {
-            startYyyymm: $('#startYyyymm').val() ,
-            endYyyymm: $('#endYyyymm').val() ,
-        };
-
-        $.ajax({
-            type: 'POST',
-            url: '/population/loadStatPopulationMoveSido',
-            dataType: 'text',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function(response){
-			var data = JSON.parse(response).data;
-			console.log(data);
-            alert('정상처리되었습니다.' + ' : ' + data);            
-            //window.location.href = '/';
-        }).fail(function(response) {
-            var error = JSON.parse(response.responseText);
-            alert(error.code + ' : ' + error.message);
-        });
-    }
 
 }
 
