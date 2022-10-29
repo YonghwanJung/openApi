109fad3c789306153b37219363de0584fccd6e68
patro <patro@112.161.194.158>
2022-10-29 19:19:07 +0900

    생산가능인구, 물가통계 적재 개발중

---
+++ src/main/java/com/apimarket/openapi/biz/price/repository/StatPriceRepository.java
@@ -0,0 +1,37 @@
+package com.apimarket.openapi.biz.price.repository;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.apimarket.openapi.biz.population.model.PopulationJuminSidoListVO;
+import com.apimarket.openapi.biz.population.model.PopulationTrendSidoListVO;
+import com.apimarket.openapi.biz.population.model.StatPopulationMoveSidoVO;
+import com.apimarket.openapi.biz.population.model.StatPopulationTrendSidoVO;
+import com.apimarket.openapi.biz.population.model.UploadStatListVO;
+
+@Repository
+public class StatPriceRepository {
+
+    private static final String MAPPER_NAME_SPACE = "mapper.StatPriceMapper.";
+
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    public int insertStatCpiForLiving(HashMap<String, String> paramMap) {
+        return sqlSessionTemplate.insert(MAPPER_NAME_SPACE + "insertStatCpiForLiving" , paramMap);
+    }
+
+    public int selectExistChkStatCpiForLiving(String yyyymm) {
+        return sqlSessionTemplate.selectOne(MAPPER_NAME_SPACE + "selectExistChkStatCpiForLiving" , yyyymm);
+    }
+
+    public int deleteStatCpiForLiving(String yyyymm) {
+        return sqlSessionTemplate.delete(MAPPER_NAME_SPACE + "deleteStatCpiForLiving" , yyyymm);
+    }
+
+
+}
